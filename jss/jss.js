912ecef00839d95c588803475c9f1131f4fb57ca
Neratu7 <43246240+Neratu7@users.noreply.github.com>
2026-09-08 22:57:05 -0500

    door

--- jss/jss.js
+++ jss/jss.js
@@ -2,8 +2,8 @@ var picture;
 var on;
 var off;
 var state;
-doorClosed = "../Images/DoorClosed2.png";
-doorOpen = "../Images/OpenDoor2.png";
+
+
 
 setInterval(function()
 {
@@ -15,12 +15,18 @@ setInterval(function()
 
 $(document).ready(function()       
 {
+	lightOff = document.getElementById("lightOff");
+	lightOn = document.getElementById("lightOn");
+	doorSound = document.getElementById("doorSound");
     door = document.getElementById("Door");
     picture = document.getElementById("myImage");
     buttonFlick = document.getElementById("flick");
     thing = document.getElementById("words");
+	
     on = "../Images/pic_bulbon.gif";
     off = "../Images/pic_bulboff.gif";
+	doorClosed = "../Images/DoorClosed2.png";
+	doorOpen = "../Images/OpenDoor2.png";
 
     /*
     var garb = state;
@@ -30,39 +36,57 @@ $(document).ready(function()
     $("#on").click(function()
         {
         document.getElementById("myImage").src= on;
+		if (buttonFlick.innerHTML != "Flick off")
+			lightOn.play();
+		
         buttonFlick.innerHTML = "Flick off";
+
 		
 		document.getElementById("Door").style.display = "block";
 		document.getElementById("Door").src = doorClosed;
+		
+		
          
         });
     $("#off").click(function()
         {
         document.getElementById("myImage").src= off;
-        buttonFlick.innerHTML = "Flick on";
+
+		if (buttonFlick.innerHTML != "Flick on")
+			lightOff.play();
 		
+		buttonFlick.innerHTML = "Flick on";
 		document.getElementById("Door").style.display = "none";
         });
     $("#flick").click(function(c)
-        {
-
-        if(buttonFlick.innerHTML == "Flick off")
-        {      
+		{
+		if(buttonFlick.innerHTML == "Flick off")
+		{
             document.getElementById("myImage").src = "../Images/pic_bulboff.gif";
             buttonFlick.innerHTML = "Flick on";
+			lightOff.play();
 			document.getElementById("Door").style.display = "none";
+
         }
         else
         {
             document.getElementById("myImage").src = "../Images/pic_bulbon.gif";
             buttonFlick.innerHTML = "Flick off";
+			lightOn.play();
 			document.getElementById("Door").style.display = "block";
         }
         });
 	$("#Door").click(function()
 	{
-		document.getElementById("Door").src = doorOpen;
+		//if(document.getElementById("Door").src.getAttribute("src") == doorClosed)
+		//{
+			document.getElementById("Door").src = doorOpen;
+			doorSound.play();
+		//}
+
 	});
+	
+	
     
     
     
