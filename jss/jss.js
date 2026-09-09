6c7f5cb3806a6717d29f62160d3af4f35eef081a
Neratu7 <43246240+Neratu7@users.noreply.github.com>
2026-09-09 14:20:46 -0500

    riddle
    rid

--- jss/jss.js
+++ jss/jss.js
@@ -15,9 +15,11 @@ setInterval(function()
 
 $(document).ready(function()       
 {
+	sphinx = document.getElementById("Sphinx");
 	lightOff = document.getElementById("lightOff");
 	lightOn = document.getElementById("lightOn");
 	doorSound = document.getElementById("doorSound");
+	talking = document.getElementById("talking");
     door = document.getElementById("Door");
     picture = document.getElementById("myImage");
     buttonFlick = document.getElementById("flick");
@@ -27,6 +29,7 @@ $(document).ready(function()
     off = "../Images/pic_bulboff.gif";
 	doorClosed = "../Images/DoorClosed2.png";
 	doorOpen = "../Images/OpenDoor2.png";
+	//sphinx = "../Images/Sphinx2.png";
 
     /*
     var garb = state;
@@ -43,7 +46,8 @@ $(document).ready(function()
 
 		
 		document.getElementById("Door").style.display = "block";
-		document.getElementById("Door").src = doorClosed;
+
+
 		
 		
          
@@ -78,16 +82,65 @@ $(document).ready(function()
         });
 	$("#Door").click(function()
 	{
-		//if(document.getElementById("Door").src.getAttribute("src") == doorClosed)
-		//{
+		if(document.getElementById("Door").getAttribute("src") == doorClosed)
+		{
 			document.getElementById("Door").src = doorOpen;
 			doorSound.play();
-		//}
+			document.getElementById("Sphinx").style.display = "block";
+
+		}
+		else if(document.getElementById("Door").getAttribute("src") == doorOpen)
+		{
+			document.getElementById("Door").src = doorClosed;
+			doorSound.play();
+			document.getElementById("Sphinx").style.display = "none";
+
+		}
+	
+
+		
+
 
 	});
 	
+	$("#Sphinx").click(function()
+	{
+		talking.play();
+		document.getElementById("riddleBox").style.display = "block";
+		document.getElementById("answerArea").style.display = "block";
+	});
 	
-    
+	
+    $("#answerButton").click(function()
+	{
+		let answer = document.getElementById("riddleAnswer").value.trim().toLowerCase();
+		
+		const acceptedAnswers=
+		[
+		"man",
+		"a man",
+		"human",
+		"a human",
+		"human being",
+		"a human being",
+		"person",
+		"a person",
+		"people",
+		"woman",
+		"a woman",
+		"humanity"
+		];
+		
+		if (acceptedAnswers.includes(answer))
+		{
+			alert("Correct!");
+			document.getElementById("bitBurner").style.display = "block";
+		}
+		else
+		{
+			alert("Incorrect!");
+		}
+	});
     
     
 });
